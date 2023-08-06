c7742abeaafd36dcebb08f25d8179dc11dba5f31
deniskrasn <kra5den@yandex.ru>
2023-08-06 09:59:41 +0300

    Commit3

--- src/main/java/ru/job4j/New.java
+++ src/main/java/ru/job4j/New.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class New {
     public static void main(String[] args) {
-        System.out.println("Hello java 2!");
+        System.out.println("Hello java 3!");
     }
 }
